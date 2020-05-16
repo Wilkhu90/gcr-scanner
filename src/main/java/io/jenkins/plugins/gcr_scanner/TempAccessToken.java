2be1e9c786ea61a28128f664ab3d57cc675e5066
Sumeet Wilkhu <sumeet.wilkhu@gmail.com>
2020-05-16 03:22:09 -0400

    Adding new logic for cred creation

---
+++ src/main/java/io/jenkins/plugins/gcr_scanner/TempAccessToken.java
@@ -0,0 +1,31 @@
+package io.jenkins.plugins.gcr_scanner;
+
+public class TempAccessToken {
+    private String access_token;
+    private Long expires_in;
+
+    public TempAccessToken() {
+    }
+
+    public TempAccessToken(String access_token, Long expires_in) {
+        this.access_token = access_token;
+        this.expires_in = expires_in;
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public Long getExpires_in() {
+        return expires_in;
+    }
+
+    public void setExpires_in(Long expires_in) {
+        this.expires_in = expires_in;
+    }
+}
+
